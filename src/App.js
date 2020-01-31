806621c7d06a48d408fa1c3090967d3eea859acb
Nick Gulson <nicholasgulson2021@u.northwestern.edu>
2020-01-30 19:28:51 -0600

    Firebase Database and Hosting added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import 'firebase/database';
+
 
 function App() {
   return (
@@ -23,4 +26,19 @@ function App() {
   );
 }
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBdTkdXSYAUdiiXAFlVaaAgJ_QQaZfdyHI",
+  authDomain: "onthego-101.firebaseapp.com",
+  databaseURL: "https://onthego-101.firebaseio.com",
+  projectId: "onthego-101",
+  storageBucket: "onthego-101.appspot.com",
+  messagingSenderId: "768309926661",
+  appId: "1:768309926661:web:7ff6e9a9e622d97c3f756c",
+  measurementId: "G-8RNRCGX4PZ"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database().ref();
+
+export {db};
 export default App;
