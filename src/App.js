7bc86f8b20b0d89ccda7a3f61df795ae74c6c805
AakashKhuranaNU <56139412+AakashKhuranaNU@users.noreply.github.com>
2020-01-30 19:30:11 -0600

    intial background

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
+import Background from './data/img.jpg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+var sectionStyle = {
+  width: "100%",
+  height: "2000px",
+  backgroundImage: `url(${Background})`
+};
+
+const App = () =>  (
+  <div>
+    <h1> On The Go : Our Travel Expert </h1>
+    <section style={ sectionStyle }>
+      </section>
+  </div>
+);
 
 export default App;
